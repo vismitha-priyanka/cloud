f3687a432b51f367786f86a98705c42f97a4b4d7
vismitha-priyanka <130166239+vismitha-priyanka@users.noreply.github.com>
2024-08-21 13:05:30 +0530

    Update vismitha.py

--- vismitha.py
+++ vismitha.py
@@ -15,8 +15,8 @@ import numpy as np
 import pandas as pd
 import sklearn
 from sklearn import linear_model
-height=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0]]
-weight=[  8,10 , 12, 14, 16,  18, 20]
+height=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0],[11.0]]
+weight=[  8,10 , 12, 14, 16,  18, 20,22]
 plt.scatter(height,weight,color='black')
 plt.xlabel("height")
 plt.ylabel("weight")
